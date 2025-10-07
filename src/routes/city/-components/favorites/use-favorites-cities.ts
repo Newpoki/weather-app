617015ab5e7fab37afb72749a96a8b4442c61386
Jason Savelli <savellijason@gmail.com>
2025-10-07 19:26:02 +0200

    feat: add some usefull cache on city get endpoint

--- src/routes/city/-components/favorites/use-favorites-cities.ts
+++ src/routes/city/-components/favorites/use-favorites-cities.ts
@@ -1,5 +1,6 @@
-import { citySchema } from "@/api/fetch-city-by-id";
+import { citySchema, fetchCityByIdQueryOption } from "@/api/fetch-city-by-id";
 import { useLocalStorage } from "@/lib/use-local-storage";
+import { useQueryClient } from "@tanstack/react-query";
 import { useCallback, useMemo, useState } from "react";
 import z from "zod";
 
@@ -13,14 +14,29 @@ export type FavoriteCity = z.infer<typeof favoriteCitySchema>;
 export type FavoriteCities = z.infer<typeof favoriteCitiesSchema>;
 
 export const useFavoritesCities = () => {
+  const queryClient = useQueryClient();
+
   const favoriteCitiesLS = useLocalStorage(
     "favorite-city",
     favoriteCitiesSchema.nullable(),
   );
 
-  const [favoriteCities, setFavoriteCities] = useState(
-    favoriteCitiesLS.get() ?? {},
-  );
+  const [favoriteCities, setFavoriteCities] = useState(() => {
+    const favoriteCitiesFromLS = favoriteCitiesLS.get() ?? {};
+
+    // As city data (not the forecast) shouldn't really evolved at all,
+    // It's safe to initialize query data from local storage
+    Object.values(favoriteCitiesFromLS).forEach((city) => {
+      if (city != null) {
+        queryClient.setQueryData(
+          fetchCityByIdQueryOption(city.id).queryKey,
+          city,
+        );
+      }
+    });
+
+    return favoriteCitiesFromLS;
+  });
 
   const handleToggleCity = useCallback(
     (city: FavoriteCity) => {
